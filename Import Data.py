9e34e0c7842f74c6d85d243eec0e3a29d08abe51
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2020-11-23 12:09:45 +0100

    init Import Data.py

--- Import Data.py
+++ Import Data.py
@@ -1,3 +1,16 @@
 import h5py
 import numpy as np
 
+path = "/data/"
+# Read in training data & assign features & labels
+data_train = h5py.File((path + 'training.h5'), 'r')
+x_tra = np.array(data_train['sen2'])
+y_tra = np.array(data_train['label'])
+
+# Read in validation data & assign features & labels
+data_test = h5py.File((path + 'validation.h5'), 'r')
+x_test = np.array(data_test['sen2'])
+y_test = np.array(data_test['label'])
+
+print(x_tra[1])
+
